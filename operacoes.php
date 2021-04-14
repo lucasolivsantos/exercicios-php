d272d56b86e4706f1ac6fc66d64fda10700145bf
Lucas Santos <lucas_olivsantos@outlook.com>
2021-04-14 14:14:32 -0300

    Primeiro exemplo de operações matemáticas

---
+++ operacoes.php
@@ -0,0 +1,9 @@
+<?php
+
+$idade = 19;
+
+$idadeDaqui10Anos = $idade + 10;
+echo "Daqui a 10 anos terei: $idadeDaqui10Anos anos." . "\n";
+
+$idadeHa5Anos = $idade - 5;
+echo "Há 5 anos atrás eu tinha: $idadeHa5Anos anos." . "\n";
