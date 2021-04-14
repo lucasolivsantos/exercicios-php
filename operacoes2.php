c53d0204fdb1baab7e14e1c984699612a3f2e2f2
Lucas Santos <lucas_olivsantos@outlook.com>
2021-04-14 14:22:04 -0300

    Segundo exemplo de operações matemáticas

---
+++ operacoes2.php
@@ -0,0 +1,15 @@
+<?php
+
+// Operadores matemáticos:
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+
+// Exponenciação
+$doisAoCubo = 2 ** 3;
+
+$restoDaDivisao = 10 % 3;
+
+echo "Resto da divisão 10 / 3 = $restoDaDivisao \n";
